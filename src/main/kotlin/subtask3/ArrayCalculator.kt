a0581565060da3811781ed4018c8443971912672
asafrona <aliaksei.safronau@etrade.com>
2020-05-03 19:49:38 +0300

    complete task 3

--- src/main/kotlin/subtask3/ArrayCalculator.kt
+++ src/main/kotlin/subtask3/ArrayCalculator.kt
@@ -2,8 +2,37 @@ package subtask3
 
 class ArrayCalculator {
 
-    // TODO: Complete the following function
     fun maxProductOf(numberOfItems: Int, itemsFromArray: Array<Any>): Int {
-        throw NotImplementedError("Not implemented")
+        var intList = itemsFromArray.filterIsInstance<Int>()
+        if (intList.isNotEmpty()) {
+            intList = intList.sortedWith(Comparator.comparingInt(Math::abs)).reversed()
+            var result = 1
+
+            if (numberOfItems < intList.size) {
+                val positiveList = (intList.filter { e: Int -> e >= 0 }).toMutableList()
+                var lastNegativeIndex = -1
+
+                for (i in 0 until numberOfItems) {
+                    val intToMultiply = intList[i]
+                    result *= intToMultiply
+
+                    positiveList.remove(intToMultiply)
+                    if (intToMultiply < 0) {
+                        lastNegativeIndex = i
+                    }
+                }
+
+                if ((result < 0) && (lastNegativeIndex >= 0) && (positiveList.isNotEmpty())) {
+                    result = result / intList[lastNegativeIndex] * positiveList.first()
+                }
+                return result
+            } else {
+                for (item in intList) {
+                    result *= item
+                }
+                return result
+            }
+        }
+        return 0
     }
 }
